92f90ad04a9d5a99e5725b7cb0a8ae6686b8442f
Arthur Gunn <arthur@gunn.co.nz>
2020-04-11 20:08:08 +1200

    Create a simple react app with parcel for packaging.

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+declare const module
+module?.hot?.accept()
+
+ReactDOM.render(
+  <p>Hello</p>,
+  document.getElementById('root')
+)
