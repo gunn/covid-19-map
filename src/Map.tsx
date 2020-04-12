feb09857e2081a33606a10b25d260d6a53f850c4
Arthur Gunn <arthur@gunn.co.nz>
2020-04-12 17:16:46 +1200

    Add map config, logic to switch map data.

--- src/Map.tsx
+++ src/Map.tsx
@@ -5,8 +5,9 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { taskMiddleware } from 'react-palm/tasks'
 import { Provider, useDispatch } from 'react-redux'
 
+import KeplerGlSchema from 'kepler.gl/schemas'
 import keplerGlReducer from 'kepler.gl/reducers'
-import { addDataToMap } from 'kepler.gl/actions'
+import { addDataToMap, updateMap } from 'kepler.gl/actions'
 
 
 const reducers = combineReducers({
@@ -26,33 +27,54 @@ export default (props: MapProps)=> (
 )
 
 
+let firstRun = true
+
 const Map = React.memo(({data}: MapProps)=> {
   const dispatch = useDispatch()
   const [_, forceUpdate] = React.useState(0)
 
   React.useEffect(()=> {
+    dispatch(addDataToMap(initialMapToLoad))
+
     window.addEventListener("resize", ()=> forceUpdate(Math.random()))
   }, [])
 
   React.useEffect(() => {
-    if (data) {
+    if (data?.rows.length) {
+      const currentConfig = KeplerGlSchema.getConfigToSave(store.getState().keplerGl["covid"])
+      const mapToLoad = firstRun ? initialMapToLoad : {}
+      firstRun = false
+
       dispatch(
         addDataToMap({
+          ...mapToLoad,
+
           datasets: {
+            data,
             info: {
               label: "COVID data",
-              id: "covid-data"
-            },
-            data
+              id: "covid-data",
+              "color": [
+                187,
+                0,
+                0
+              ]
+            }
           },
-          option: {
-            centerMap: true,
-            readOnly: false
-          },
-          config: {}
+          // option: {
+          //   keepExistingConfig: true,
+          //   centerMap: false,
+          //   readOnly: true
+          // }
         })
-      );
+      )
+
+
+      dispatch(
+        updateMap((firstRun ? config : currentConfig).config.mapState)
+      )
     }
+
   }, [dispatch, data])
 
   return (
@@ -64,3 +86,154 @@ const Map = React.memo(({data}: MapProps)=> {
     />
   )
 })
+
+
+const config = {
+  "version": "v1",
+  "config": {
+    "visState": {
+      "filters": [],
+      "layers": [
+        {
+          "id": "hrtxigu",
+          "type": "hexagon",
+          "config": {
+            "dataId": "covid-data",
+            "label": "new layer",
+            "color": [
+              130,
+              154,
+              227
+            ],
+            "columns": {
+              "lat": "lat",
+              "lng": "long"
+            },
+            "isVisible": true,
+            "visConfig": {
+              "opacity": 0.8,
+              "worldUnitSize": 16,
+              "resolution": 8,
+              "colorRange": {
+                "name": "Global Warming",
+                "type": "sequential",
+                "category": "Uber",
+                "colors": [
+                  "#5A1846",
+                  "#900C3F",
+                  "#C70039",
+                  "#E3611C",
+                  "#F1920E",
+                  "#FFC300"
+                ]
+              },
+              "coverage": 1,
+              "sizeRange": [
+                0,
+                1000
+              ],
+              "percentile": [
+                0,
+                100
+              ],
+              "elevationPercentile": [
+                0,
+                100
+              ],
+              "elevationScale": 82.8,
+              "colorAggregation": "sum",
+              "sizeAggregation": "average",
+              "enable3d": true
+            },
+            "textLabel": [
+              {
+                "field": null,
+                "color": [
+                  255,
+                  255,
+                  255
+                ],
+                "size": 18,
+                "offset": [
+                  0,
+                  0
+                ],
+                "anchor": "start",
+                "alignment": "center"
+              }
+            ]
+          },
+          "visualChannels": {
+            "colorField": {
+              "name": "cases",
+              "type": "integer"
+            },
+            "colorScale": "quantile",
+            "sizeField": {
+              "name": "deathsPer100K",
+              "type": "real"
+            },
+            "sizeScale": "linear"
+          }
+        }
+      ],
+      "interactionConfig": {
+        "tooltip": {
+          "fieldsToShow": {
+            "covid-data": [
+              "state",
+              "subregion",
+              "long",
+              "population",
+              "cases"
+            ]
+          },
+          "enabled": true
+        },
+        "brush": {
+          "size": 0.5,
+          "enabled": false
+        },
+        "coordinate": {
+          "enabled": false
+        }
+      },
+      "layerBlending": "normal",
+      "splitMaps": [],
+      "animationConfig": {
+        "currentTime": null,
+        "speed": 1
+      }
+    },
+    "mapState": {
+      "bearing": 0,
+      "dragRotate": false,
+      "latitude": 39.15690502140835,
+      "longitude": -104.29845290568925,
+      "pitch": 0,
+      "zoom": 3.9304282529358545,
+      "isSplit": false
+    },
+    "mapStyle": {
+      "styleType": "dark",
+      "topLayerGroups": {},
+      "visibleLayerGroups": {
+        "label": false,
+        "road": true,
+        "border": false,
+        "building": true,
+        "water": true,
+        "land": true,
+        "3d building": false
+      },
+      "threeDBuildingColor": [
+        9.665468314072013,
+        17.18305478057247,
+        31.1442867897876
+      ],
+      "mapStyles": {}
+    }
+  }
+}
+
+const initialMapToLoad = KeplerGlSchema.load([], config)
